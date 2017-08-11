6912e746ff488cb7262950bb7764b89128b6b186
Andrés Saraos Luna <andres.saraos-luna@imie.fr>
2017-08-11 16:10:08 +0200

    Uses the QueryBuilder.

--- examples/01-basic_get.rs
+++ examples/01-basic_get.rs
@@ -1,19 +1,31 @@
 extern crate gwent_api;
 
 use gwent_api::client::gw_client::Client;
-use gwent_api::model::card::Card;
+use gwent_api::model::card::CardPage;
+use gwent_api::model::request::*;
 
 fn main() {
     println!{"Basic get example"};
 
     let client = Client::new();
 
-    let mut response = client.get("https://api.gwentapi.com/v0/cards/gycZySRxV-2wgUFMzdNuFw").unwrap();
+    let c_request = CardPageRequest {
+        lang: Lang::fr_FR,
+        limit: 150,
+        name: None,
+        offset: 3,
+        if_modified_since: None
+    };
+    let query = QueryBuilder::query("https://api.gwentapi.com/v0/cards", c_request.to_hash());
+
+    println!("{:?}", query);
+
+    let mut response = client.get(query.unwrap().as_str()).unwrap();
 
     println!("{}", response.status());
     println!("{:?}", response);
 
-    match response.json::<Card>() {
+    match response.json::<CardPage>() {
         Err(e) => println!("ERROR: {}", e),
         Ok(card) => println!("{:#?}", card)
     };
