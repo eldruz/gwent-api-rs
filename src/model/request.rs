57fa57d7dc797a3993b1ad92be2804757cc90816
Andrés Saraos Luna <andres.saraos-luna@imie.fr>
2017-08-11 16:09:22 +0200

    Starts adding queries handler for GET requests.

---
+++ src/model/request.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+pub enum Lang {
+    en_US,
+    de_DE,
+    es_ES,
+    es_MX,
+    fr_FR,
+    it_IT,
+    ja_JP,
+    pl_PL,
+    pt_BR,
+    ru_RU,
+}
+
+impl Lang {
+    pub fn as_str(&self) -> &str {
+        match self {
+            &Lang::en_US => "en-US",
+            &Lang::de_DE => "de-DE",
+            &Lang::es_ES => "es-ES",
+            &Lang::es_MX => "es-MX",
+            &Lang::fr_FR => "fr-FR",
+            &Lang::it_IT => "it-IT",
+            &Lang::ja_JP => "ja-JP",
+            &Lang::pl_PL => "pl-PL",
+            &Lang::pt_BR => "pt-BR",
+            &Lang::ru_RU => "ru-RU",
+        }
+    }
+}
+
+pub struct CardPageRequest {
+    pub lang: Lang,
+    pub limit: u64,
+    pub name: Option<String>,
+    pub offset: u64,
+    pub if_modified_since: Option<String>,
+}
+
+impl CardPageRequest {
+    pub fn default() -> CardPageRequest {
+        CardPageRequest {
+            lang: Lang::en_US,
+            limit: 20,
+            name: None,
+            offset: 0,
+            if_modified_since: None
+        }
+    }
+
+    pub fn to_hash(&self) -> HashMap<String, String> {
+        let mut hash = HashMap::new();
+        hash.insert(String::from("lang"), String::from(self.lang.as_str()));
+        hash.insert(String::from("limit"), self.limit.to_string());
+        if self.name.is_some() {
+            let name = self.name.clone().unwrap();
+            if name.len() >= 3 {
+                hash.insert(String::from("name"), name);
+            }
+        }
+        hash.insert(String::from("offset"), self.offset.to_string());
+        hash
+    }
+}
+
+pub struct QueryBuilder {}
+
+impl QueryBuilder {
+    pub fn query(uri: &str, args: HashMap<String, String>) -> Result<String, &'static str> {
+        let mut query = String::from(uri);
+        query.push_str("?");
+        for (key, val) in &args {
+            query.push_str(key);
+            query.push_str("=");
+            query.push_str(val);
+            query.push_str("&");
+        }
+        Ok(query)
+    }
+}
