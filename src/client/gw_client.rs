bfd54f1e6a673f6a053f0249dd75ed3129feb128
Andrés Saraos Luna <eldruz@protonmail.com>
2017-08-21 16:55:09 +0200

    Adds a QueryBuilder
    The builder gives a valid URL, but there is still a lot of checks that need to be made for it to be fully functional.
    I'm keeping it as is as of now.

--- src/client/gw_client.rs
+++ src/client/gw_client.rs
@@ -2,6 +2,7 @@ extern crate reqwest;
 
 use model::card::{Card, CardPage, Variation};
 use self::reqwest::Url;
+use std::collections::HashMap;
 
 macro_rules! get_card {
     ($query: expr, $query_type: ty) => (
@@ -17,13 +18,40 @@ macro_rules! get_card {
     )
 }
 
+pub struct QueryBuilder(pub HashMap<String, String>);
+
+impl QueryBuilder {
+    pub fn new() -> QueryBuilder {
+        QueryBuilder(HashMap::new())
+    }
+
+    pub fn as_url(&self, base_url: &str) -> Result<Url, reqwest::UrlError> {
+        Url::parse_with_params(base_url, &self.0)
+    }
+
+    pub fn lang<'a>(&'a mut self, language: &str) -> &'a mut Self {
+        self.0.insert(String::from("lang"), String::from(language));
+        self
+    }
+
+    pub fn card_name<'a>(&'a mut self, card_name: &str) -> &'a mut Self {
+        self.0.insert(String::from("name"), String::from(card_name));
+        self
+    }
+
+    pub fn with_arg<'a>(&'a mut self, param: &str, value: &str) -> &'a mut Self {
+        self.0.insert(String::from(param), String::from(value));
+        self
+    }
+}
 
 pub struct Client { }
 
 impl Client {
     fn get_query_card_by_name(card_name: &str) -> Result<Url, reqwest::UrlError> {
-        Url::parse_with_params("https://api.gwentapi.com/v0/cards",
-            &[("name", card_name)])
+        QueryBuilder::new()
+            .card_name(card_name)
+            .as_url("https://api.gwentapi.com/v0/cards/")
     }
 
     pub fn get_card_by_name(card_name: &str) -> reqwest::Result<Card> {
